c0b9e94bb2b55ccba261cf11a89ad3571ff16eca
Rasmus Jonsson <rasmusjons@gmail.com>
2019-11-12 01:06:39 +0100

    first c

---
+++ src/MilitaryService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rasmus
+ */
+public class MilitaryService implements NationalService {
+
+    private int daysLeft;
+
+    public MilitaryService(int daysLeft) {
+        this.daysLeft = daysLeft;
+
+    }
+
+    public int getDaysLeft() {
+
+        return daysLeft;
+    }
+    
+    
+    
+    public void work() {
+        if (daysLeft > 0) {
+            daysLeft--;
+        }
+
+    }
+}
